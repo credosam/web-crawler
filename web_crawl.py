d3ad92036402525dc40fa62abbc33a1284ce2fa5
Samarth Goel <ug201210031@iitj.ac.in>
2014-06-10 16:47:14 +0530

    My web crawler

---
+++ web_crawl.py
@@ -0,0 +1,45 @@
+import urllib
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1: 
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+def union(p,q):
+    for e in q:
+        if e not in p:
+            p.append(e)
+
+
+def get_all_links(page):
+    links = []
+    while True:
+        url,endpos = get_next_target(page)
+        if url:
+            links.append(url)
+            page = page[endpos:]
+        else:
+            break
+    return links
+
+def crawl_web(seed):
+    tocrawl = [seed]
+    crawled = []
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled:
+            union(tocrawl, get_all_links(page))
+            crawled.append(page)
+    return crawled
+def print_links(seed):
+    lis = []
+    lis = crawl_web(seed)
+    for e in lis:
+        print e
+    
+s = raw_input()
+print_links(urllib.urlopen(s).read())
